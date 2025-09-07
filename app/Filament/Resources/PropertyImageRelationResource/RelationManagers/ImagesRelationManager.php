926242cc084fc04aa9c8ad37fe957a3f32c5aa04
luewire <bimozen22@gmail.com>
2025-09-07 15:38:51 +0700

    exits

--- app/Filament/Resources/PropertyImageRelationResource/RelationManagers/ImagesRelationManager.php
+++ app/Filament/Resources/PropertyImageRelationResource/RelationManagers/ImagesRelationManager.php
@@ -1,1 +1,52 @@
-<?php // file moved to App\Filament\Resources\PropertyResource\RelationManagers\ImagesRelationManager.php
+<?php
+
+namespace App\Filament\Resources\PropertyResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ImagesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'images';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image_url')
+                    ->label('Property Image')
+                    ->image()
+                    ->directory('properties') // folder di storage/app/public/properties
+                    ->required(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('image_url')
+            ->columns([
+                Tables\Columns\ImageColumn::make('image_url')
+                    ->label('Image')
+                    ->square(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
